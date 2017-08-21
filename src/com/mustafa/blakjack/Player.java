c0c03f412ee5f6b5e4b4591bf8f15ba8202355a5
mustafa alici <mustafaalici@mustafas-MacBook-Pro.local>
2017-08-21 13:43:40 -0600

    added cash to player

--- src/com/mustafa/blakjack/Player.java
+++ src/com/mustafa/blakjack/Player.java
@@ -7,6 +7,7 @@ public class Player implements Actor {
 	
 	private GameHand hand;
 	private String name;
+	private double cash;
 
 	public Player(String name) {
 		super();
@@ -43,6 +44,14 @@ public class Player implements Actor {
 		return name;
 	}
 
+	public double getCash() {
+		return cash;
+	}
+
+	public void setCash(double cash) {
+		this.cash = cash;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -50,9 +59,46 @@ public class Player implements Actor {
 		builder.append(hand);
 		builder.append(", name=");
 		builder.append(name);
+		builder.append(", cash=");
+		builder.append(cash);
 		builder.append("]");
 		return builder.toString();
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(cash);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((hand == null) ? 0 : hand.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Player other = (Player) obj;
+		if (Double.doubleToLongBits(cash) != Double.doubleToLongBits(other.cash))
+			return false;
+		if (hand == null) {
+			if (other.hand != null)
+				return false;
+		} else if (!hand.equals(other.hand))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 }
